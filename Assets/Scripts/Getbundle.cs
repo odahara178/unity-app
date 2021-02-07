82240f15e9c1268b52fd4194887c61683dee5e05
odahara178 <odahara178@gmail.com>
2021-02-07 15:16:59 +0900

    assetbundle prefab表示

---
+++ Assets/Scripts/Getbundle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Getbundle : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(DownLoad());
+    }
+
+    IEnumerator DownLoad()
+    {
+        string bundleUrl = Application.streamingAssetsPath + "/yuusya";
+        AssetBundleCreateRequest request = AssetBundle.LoadFromFileAsync(bundleUrl);
+        while (!request.isDone)
+        {
+            yield return null;
+        }
+        AssetBundle assetBundle = request.assetBundle;
+        AssetBundleRequest image = assetBundle.LoadAssetAsync<GameObject>("main");
+
+        GameObject obj = (GameObject)Instantiate(image.asset);
+        obj.transform.SetParent(transform, false);
+        obj.GetComponent<RectTransform>().anchoredPosition = new Vector2(2, 2);
+    }
+}
---
+++ Assets/Scripts/TestLoad.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestLoad : MonoBehaviour
+{
+
+    void Start()
+    {
+        //AssetBundlePrefab();
+        AssetBundleImage();
+    }
+
+    void AssetBundleImage()
+    {
+        AssetBundle image = AssetBundle.LoadFromFile("Assets/StreamingAssets/Image/yuusya");
+        if (image == null)
+        {
+            // ロード失敗
+            Debug.Log("error : load bundle");
+        }
+        else
+        {
+            // ロード成功
+            Debug.Log("success : load bundle");
+
+
+            Image ist = image.LoadAsset<Image>("main.png");
+            if (ist == null)
+            {
+                //// Prefabのロード失敗
+                Debug.Log("error : load asset");
+            }
+            else
+            {
+                Debug.Log("success : load asset");
+
+                //var _image = this.GetComponent<Image>();
+                //_image.sprite = ist;
+                Instantiate(image);
+            }
+        }
+    }
+
+
+    void AssetBundlePrefab()
+    {
+        // アセットバンドルのロード
+        AssetBundle ab = AssetBundle.LoadFromFile("Assets/StreamingAssets/Android/prefab");
+
+        if (ab == null)
+        {
+            //// ロード失敗
+            //Debug.Log("error : load bundle");
+        }
+        else
+        {
+            //// ロード成功
+            //Debug.Log("success : load bundle");
+
+            // Prefabのロード
+            GameObject prefab = ab.LoadAsset<GameObject>("sd");
+
+            if (prefab == null)
+            {
+                //// Prefabのロード失敗
+                //Debug.Log("error : load asset");
+            }
+            else
+            {
+                // Prefabのロード成功
+
+                // Prefabをインスタンス化
+                Instantiate(prefab);
+            }
+        }
+    }
+}
